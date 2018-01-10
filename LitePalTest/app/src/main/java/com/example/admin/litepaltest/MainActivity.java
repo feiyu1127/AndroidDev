58d6e1c450b49eb57b6914ba8a4dee65eb5ee7fb
feiyu <15209272743@163.com>
2018-01-10 23:10:25 +0800

    LitePal fixed

--- LitePalTest/app/src/main/java/com/example/admin/litepaltest/MainActivity.java
+++ LitePalTest/app/src/main/java/com/example/admin/litepaltest/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.admin.litepaltest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.litepal.crud.DataSupport;
 import org.litepal.tablemanager.Connector;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +23,49 @@ public class MainActivity extends AppCompatActivity {
         Button createDatabase = findViewById(R.id.create_database);
         Button addData = findViewById(R.id.add_data);
         Button updateData = findViewById(R.id.update_data);
+        Button deleteData = findViewById(R.id.delete_data);
+        Button queryData = findViewById(R.id.query_data);
+
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Book> books = DataSupport.findAll(Book.class);
+                Log.d("循环外", "onClick: ");
+                for(Book book:books){
+                    Log.d(TAG, "书的名字是" + book.getName());
+                    Log.d(TAG, "循环内");
+                }
+            }
+        });
+
+        
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataSupport.deleteAll(Book.class,"price < ?","15");
+            }
+        });
+
 
         updateData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Book book = new Book();
-                book.setName("The Lost Symbol");
-                book.setAuthor("Dan Brown");
-                book.setPages(510);
-                book.setPrice(16.95);
-                book.setPress("UnKnow");
-                book.save();
+//                Book book = new Book();
+//                book.setName("The Lost Symbol");
+//                book.setAuthor("Dan Brown");
+//                book.setPages(510);
+//                book.setPrice(16.95);
+//                book.setPress("UnKnow");
+//                book.save();
+//
+//                book.setPrice(10.99);
+//                book.save();
 
-                book.setPrice(10.99);
-                book.save();
+                Book book = new Book();
+                book.setPrice(250);
+                book.setPress("DaHua");
+                book.updateAll("name=? and author=?","The Lost Symbol","Dan Brown");
+
             }
         });
 
