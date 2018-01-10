dce17fc115d8570e515db9214c44737f0a8c3e9c
蜗牛Narcissus <nwei@victtech.com>
2018-01-10 13:06:37 +0800

    SharedPreference

--- BroadcastBestPractice/app/src/main/java/com/example/admin/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/admin/broadcastbestpractice/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.admin.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
 
     private EditText accountEdit;
     private EditText passwordEdit;
@@ -22,9 +29,22 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass = findViewById(R.id.remember_pass);
+        Boolean isRemember = pref.getBoolean("remember_password",false);
+
         accountEdit = findViewById(R.id.account);
         passwordEdit = findViewById(R.id.password);
         login = findViewById(R.id.login);
+
+        if(isRemember){
+            //将账号和密码设置到文本框中
+            String account = pref.getString("account","");
+            String password = pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +53,17 @@ public class LoginActivity extends BaseActivity {
                     String password = passwordEdit.getText().toString();
                     //如果账号是 admin,密码是 123456 认为登录成功
                     if(account.equals("admin") && password.equals("123456")){
+
+                        if(rememberPass.isChecked()){
+                            editor = pref.edit();
+                            editor.putBoolean("remember_password",true);
+                            editor.putString("account",account);
+                            editor.putString("password",password);
+                        }else{
+                            editor.clear(); //将 SharedPreference 文件中的数据全部清除
+                        }
+                        editor.apply();
+
                         Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                         startActivity(intent);
                         finish();
---
+++ SharedPreferencesTest/app/src/main/java/com/example/admin/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.admin.sharedpreferencestest;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button savaData = findViewById(R.id.save_data);
+        Button restoreData = findViewById(R.id.restore_data);
+
+        savaData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age",28);
+                editor.putBoolean("married",false);
+                editor.apply();
+            }
+        });
+
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age = pref.getInt("age",0);
+                Boolean married = pref.getBoolean("married",false);
+
+                Log.d("姓名是:",name);
+            }
+        });
+
+    }
+}
